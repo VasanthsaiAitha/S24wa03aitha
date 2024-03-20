0f239552598f13b9cfea3d928d2083df57421fe9
VasanthsaiAitha <aithavasanthsai@gmail.com>
2024-03-20 12:22:06 -0500

    update bouns

--- routes/computation.js
+++ routes/computation.js
@@ -5,8 +5,8 @@ router.get('/', function(req, res, next) {
     function getBaseLog(x, y) {
       return Math.log(y) / Math.log(x);
     }
-    let x = Math.floor(Math.random() * 10) + 1;
-    let y = Math.pow(x, Math.floor(Math.random() * 3) + 2); 
+    let x = Math.random();
+    let y = Math.random(); 
     let result = getBaseLog(x, y);
     let response = `Base log of ${y} with base ${x} is ${result}`;
     res.send(response);
