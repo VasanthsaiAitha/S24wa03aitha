58ad815e0f86eb4524765df0865569efa2a3f0fb
VasanthsaiAitha <aithavasanthsai@gmail.com>
2024-03-20 12:33:47 -0500

    final push

--- routes/computation.js
+++ routes/computation.js
@@ -5,7 +5,8 @@ router.get('/', function(req, res, next) {
     function getBaseLog(x, y) {
       return Math.log(y) / Math.log(x);
     }
-    let x = Math.random();
+    
+    let x = req.query.x ? parseFloat(req.query.x) : Math.random();
     let y = Math.random(); 
     let result = getBaseLog(x, y);
     let response = `Base log of ${y} with base ${x} is ${result}`;
